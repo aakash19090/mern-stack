a9b56dfea80ad1e342a134c3ffd1c6f6079796da
Akasho2h <akash.sharma@o2h.com>
2020-10-29 18:12:50 +0530

    created views with view engine- AKash

--- app.js
+++ app.js
@@ -4,24 +4,15 @@ const app = express();
 const port = 5000;
 
 // Importing Routes
-
 require('./routes')(app);
 
 // Serving Static Files from Serverside. Useful to link external CSS or JS files to index.html file
-
 app.use(express.static('public'))
 
-
-// Sending Responses at various routes through ExpressJS at 5000 port
-
-app.get('/', (req, res)=>{
-    res.send("Hello world from Express JS")
-})
-
-
+// Setting View Engine
+app.set('view engine', 'ejs')
 
 // To allow GET requests from this server
-
 // app.use(function(req,res,next){
 //     res.setHeader('Access-Control-Allow-Origin', '*');
 //     res.setHeader('Access-Control-Allow-Methods', 'GET');
@@ -29,21 +20,6 @@ app.get('/', (req, res)=>{
 //     // Pass to the next layer of middleware
 //     next();
 // });
-
-
-// Sending Responses to CLient side (index.html File)
-
-app.get('/home', (req, res)=>{
-    res.send({
-        firstName : "Akash",
-        lastName : "Sharma"
-    })
-})
-
-// Sending whole file to Client Side
-app.get('/file', (req, res) =>{
-    res.sendFile(__dirname +'/index.html')
-})
 
 
 app.listen(port, ()=>{
--- routes.js
+++ routes.js
@@ -1,12 +1,32 @@
-// Sending responses to other routes
+// Sending Responses at various routes through ExpressJS at 5000 port
+
+const data = {
+    firstName : "Akash",
+    lastName : "Sharma",
+    occupation : "Software Developer"
+}
 
 module.exports = (app) => {
 
-    app.get('/test', (req, res)=>{
-        res.send("Just a test from Express JS")
+    app.get('/', (req, res)=>{
+        res.render('home', {data:data})
     })
     
     app.get('/about', (req, res)=>{
-        res.send("About page from Express JS")
+        res.render('about')
     })
+
+    app.get('/profile/:id', (req,res)=>{
+        res.send(`Requested User Profile is ${req.params.id}`)
+
+    })
+
+    // Sending Responses to CLient side (index.html File)
+
+
+    // Sending whole file to Client Side
+    // app.get('/file', (req, res) =>{
+    //     res.sendFile(__dirname +'/index.html')
+    // })
+
 }
