5697241b3cb07881918bdd44715dcd7bc3de84a3
Akasho2h <akash.sharma@o2h.com>
2020-10-30 21:14:36 +0530

    env variables added - AKash

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // Importing Model
 require('./models/wishlist');
